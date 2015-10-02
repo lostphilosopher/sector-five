6f5a777885ca9a7ce46c39b9d39acee8ad68cd67
lostphilosopher <wandersen02@gmail.com>
2015-10-02 16:05:58 -0500

    Running game mechanics

---
+++ bullet.rb
@@ -0,0 +1,24 @@
+class Bullet
+  SPEED = 5
+
+  attr_reader :x, :y, :radius
+
+  def initialize(window, x, y, angle)
+    @x = x
+    @y = y
+
+    @direction = angle
+    @image = Gosu::Image.new('images/bullet.png')
+    @radius = 3
+    @window = window
+  end
+
+  def draw
+    @image.draw(@x - @radius, @y - @radius, 1)
+  end
+
+  def move
+    @x += Gosu::offset_x(@direction, SPEED)
+    @y += Gosu::offset_y(@direction, SPEED)
+  end
+end
